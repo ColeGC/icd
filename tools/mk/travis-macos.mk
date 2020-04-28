856cbcd2dc93cef1a8a8a5d9d9428d5ba5fb6fc2
Jack Wasey <jack@jackwasey.com>
2020-04-28 06:38:34 -0400

    travis osx ccache

--- tools/mk/travis-macos.mk
+++ tools/mk/travis-macos.mk
@@ -6,13 +6,13 @@ icd_osx_cc = /usr/local/bin/gcc
 icd_osx_fc_ver = $(shell sh -c "command -v gfortran || command -v gfortran$(icd_osx_comp_ver) || echo 'cannot find gfortran!' >&2" )
 
 # try some prophylactic debugging:
-@echo Old CC, etc are $(CXX) $(CXX11) $(CXX14) $(CXX17) $(CXX20) $(CC) $(FC) $(F77)
+#@echo Old CC, etc are $(CXX) $(CXX11) $(CXX14) $(CXX17) $(CXX20) $(CC) $(FC) $(F77)
 
-CXX = $(CCACHE) $(icd_osx_cxx)$(icd_osx_comp_ver)
+CXX = $(icd_osx_cxx)$(icd_osx_comp_ver)
 CXX11 = $(CXX)
 CXX14 = $(CXX)
 CXX17 = $(CXX)
 CXX20 = $(CXX)
-CC := $(CCACHE) $(icd_osx_cc)$(icd_osx_comp_ver)
-FC = $(CCACHE) $(icd_osx_fc_ver)
+CC = $(icd_osx_cc)$(icd_osx_comp_ver)
+FC = $(icd_osx_fc_ver)
 F77 = $(FC)
